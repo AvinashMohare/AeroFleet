a13cbb7e8ca92246ebf57e091cb8bc0f687fbd0b
Avinash Mohare <mohareavinash80@gmail.com>
2025-06-29 15:32:06 +0530

    added create mission and view mission pages

--- client/src/routesConfig.js
+++ client/src/routesConfig.js
@@ -4,6 +4,8 @@ import AdminDashboard from "./pages/admin/AdminDashboard";
 import OperatorDashboard from "./pages/operator/OperatorDashboard";
 import Drones from "./pages/admin/Drones";
 import AddDrone from "./pages/admin/AddDrone";
+import CreateMission from "./pages/admin/CreateMission";
+import Missions from "./pages/admin/Missions";
 
 export const publicRoutes = [
   { path: "/", element: <Signup /> },
@@ -14,6 +16,8 @@ export const adminRoutes = [
   { path: "/admin-dashboard", element: <AdminDashboard /> },
   { path: "/admin/drones", element: <Drones /> },
   { path: "/admin/add-drone", element: <AddDrone /> },
+  { path: "/admin/create-mission", element: <CreateMission /> },
+  { path: "/admin/missions", element: <Missions /> },
 ];
 
 export const operatorRoutes = [
